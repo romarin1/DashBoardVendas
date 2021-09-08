0aee09121d0fbc7a6fbe74676234e2662cebb913
romarin1 <romario.os@sempreceub.com>
2021-09-08 17:49:36 -0300

    Criação do prijeto

---
+++ backend/src/main/java/com/vendas/dashboardvendas/DashboardvendasApplication.java
@@ -0,0 +1,13 @@
+package com.vendas.dashboardvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vendas/dashboardvendas/DashboardvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendas.dashboardvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
